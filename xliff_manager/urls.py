1b93fa3d6b416d8747245f1b9dd5f1362cbbcddc
Esteve Graells <esteve.graells@gmail.com>
2025-03-26 15:46:56 +0100

    20250326

--- xliff_manager/urls.py
+++ xliff_manager/urls.py
@@ -17,12 +17,15 @@ urlpatterns = [
     path('my_pending_reviews', views.choose_review_view, name='choose_review_view'),
 
     path('diary_log', views.diary_log_view, name='diary_log'),
+    path('custom_instructions', views.custom_instructions_view, name='custom_instructions'),
 
     path('download_file/<str:type>/<str:id>/<path:file_to_download>/', views.download_file, name='download_file'),
     
     path('login/', LoginView.as_view(template_name='xliff_manager/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='xliff_manager/logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'), 
     path('admin/', admin.site.urls),
+    path('userpage', views.userpage, name='userpage'),
 
     path('confirm_insertion/<int:num_records>/', views.confirm_insertion_view, name='confirm_insertion'),
 ]
